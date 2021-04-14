ce051a7503501f4e32e6ff4656f9bf902b3fdd20
Dharmang92 <dharmang.gajjar92@gmail.com>
2021-04-14 21:07:05 +0530

    added heroes component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from "./Hero";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Dharmang'},
+  {id: 2, name: 'Chizuru'},
+  {id: 3, name: 'Tsugumi'},
+  {id: 4, name: 'Shizuka'}
+]
